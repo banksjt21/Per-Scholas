9c95b5a6a6c490720dee9e013f46b666326eea86
banksjt21 <113642612+banksjt21@users.noreply.github.com>
2022-10-31 19:53:12 -0400

    updated code. works in browser and console

--- Classwork/W5D3/save-the-universe/save-the-universe.js
+++ Classwork/W5D3/save-the-universe/save-the-universe.js
@@ -68,6 +68,14 @@ function randomizerRangeRounded(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
 
+function checkForBrowser() {
+    if(typeof window === 'undefined') {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 
 //  Class Definitions
 class Ship {
@@ -101,7 +109,7 @@ class Alien {
 
 
 
-
+//  game object
 const game = {
     // arrays for actor objects
     shipGroup   : [],
@@ -124,10 +132,29 @@ const game = {
     // number of rounds
     count       : 1,
 
-    continueGame : "",
-    
+    // set the intial state of the actors to false
     shipReady : false,
     alienReady: false,
+
+    // these two properties are only used if the game is run in the browser
+    // otherwise the game runs until either all ships or all aliens are destroyed
+    continueGame : null,
+    browserPresent : checkForBrowser(),
+
+    // this method is only used if the game is run in the broswer
+    resetGame() {
+        this.currentShip     = {};
+        this.currentAlien    = {};
+        this.victor          = {};
+        this.vanquished      = {};
+        this.victorGroup     = [];
+        this.vanquishedGroup = [];
+        this.count           = 1;
+        this.shipReady       = false;
+        this.alienReady      = false;
+        this.continueGame    = null;
+        this.start();
+    },
 
 
 
@@ -212,6 +239,7 @@ const game = {
                     this.victorGroup     = this.shipGroup;
                     this.vanquishedGroup = this.alienGroup;
                     this.alienReady      = false;
+                    this.continueGame    = false;
                     console.log(`Your ship had ${this.victor.hull} hull remaining.`);
                     break;
                 }
@@ -262,31 +290,61 @@ const game = {
         this.duelBetweenCurrentActors();
 
         console.log("------------------------------\n\n");
-        // this.promptUser();
+
+
     },
 
 
-    // run battle at least once, check for victor, update vanquished with next actor in group
-    prepareNextDuel() {
-        // while both actor groups are not empty, run this code
+    // run battle at least once, check for victor, update vanquished with next actor in group, loop until game over
+    battleItOut() {
+ 
+        // this while loop only handles the initial battle and is only run once ...
+        // so it can either be turned into an IF statement or deleted
         while (this.shipReady === true && this.alienReady === true) {
 
-            // make sure that the battle occurs at least once
+            // this makes sure that the battle occurs at least once
             if(this.count === 1) {
                 this.startBattleRound();
-            }            
-
+            }   
+            
+            // this while loop handles all subsequent battles
             // while the vanquished (losing) group still has actors left, run this code
             while(this.vanquishedGroup.length > 0) {
 
                 // if the alien was vanquished, get the next alien from the group
                 if(this.vanquished === this.currentAlien) {
+                    if(this.browserPresent === true){
+                        this.continueGame = confirm("You defeated an alien!\nWould you like to continue?");
+
+                        if(!this.continueGame) {
+                            console.log("============================================================");
+                            console.log("POST-GAME REPORT");
+                            console.log("============================================================");
+                            console.log("You live to fight another day!");
+                            console.log("============================================================");
+                            break;
+                        }
+                    }
                     this.currentAlien = this.getNextAlien(this.alienGroup);
                     if(this.currentAlien) {
                         this.alienReady = true;
                     }
                 } else {
                     // the ship was vanquished, so get the next ship from the group
+                    
+
+                    if(this.browserPresent === true){
+                        this.continueGame = confirm("One of your ships was destroyed!\nPrepare the next ship?");
+
+                        if(!this.continueGame) {
+                            console.log("============================================================");
+                            console.log("POST-GAME REPORT");
+                            console.log("============================================================");
+                            console.log("You live to fight another day!");
+                            console.log("============================================================");
+                            break;
+                        }
+                    }
                     this.currentShip  = this.getNextShip(this.shipGroup);
                     if(this.currentShip) {
                         this.shipReady = true;
@@ -313,6 +371,15 @@ const game = {
                     console.log(`You didn't defeat enough of the aliens. Earth is lost ... better luck next time`);
                 }
                 console.log("============================================================");
+
+                // if running in the browser, ask user if they would like to play again
+                if(this.browserPresent === true){
+                    this.continueGame = confirm("New Game?");
+
+                    if(this.continueGame) {
+                        this.resetGame();
+                    }
+                }
             }
 
         }
@@ -337,47 +404,51 @@ const game = {
         this.currentAlien = this.getNextAlien(this.alienGroup);
 
         // battle it out
-        this.prepareNextDuel();
+        this.battleItOut();
   
     },
 
     
-    // // prompt the user to continue
+    // prompt the user to continue (NOT USED)
+    // could not figure out how to get the script to pause execution for userInput
     // promptUser() {
     //     const readline = require('readline');
     //     const rl = readline.createInterface({input : process.stdin,
     //                                         output : process.stdout});
 
-    //     let continueBattle;
                                             
     //     rl.question(`Would you like to continue? Type 'y' or 'n'\n`, function(userInput) {
     //         userInput = userInput.trim();
     //         if(userInput === "y") {
     //             console.log("Into the battle we fly!");
-    //             continueBattle = "y";
+    //             this.continueGame = true;
+    //             console.log(this.continueGame);
     //             rl.close();
-    //             return continueBattle;
+    //             return;
     //         }
     //         else if(userInput === "n") {
     //             console.log("Good choice! Live to fight another day.");
-    //             continueBattle = "n";
+    //             this.continueGame = false;
+    //             console.log(this.continueGame);
     //             rl.close();
-    //             return continueBattle;
+    //             return;
     //         } else {
     //             rl.setPrompt(`Invalid response. Please type 'y' or 'n'\n`);
     //             rl.prompt();
     //             rl.on('line', function(userInput) {
     //                 if(userInput === "y") {
     //                     console.log("Into the battle we fly!");
-    //                     continueBattle = "y";
+    //                     this.continueGame = true;
+    //                     console.log(this.continueGame);
     //                     rl.close();
-    //                     return continueBattle;
+    //                     return;
     //                 }
     //                 else if(userInput === "n") {
     //                     console.log("Good choice! Live to fight another day.");
-    //                     continueBattle = "n";
+    //                     this.continueGame = false;
+    //                     console.log(this.continueGame);
     //                     rl.close();
-    //                     return continueBattle;
+    //                     return;
     //                 } else {
     //                     rl.setPrompt(`Your answer of ${userInput} is not valid. Please type 'y' or 'n'\n`);
     //                     rl.prompt();
