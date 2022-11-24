e7e1a6dfc7d8b8c9e344fc0dc2e8fc69fe289b67
banksjt21 <113642612+banksjt21@users.noreply.github.com>
2022-11-23 20:05:58 -0500

    Created logs model

--- Homework/W9D2/captains-log/server.js
+++ Homework/W9D2/captains-log/server.js
@@ -6,6 +6,7 @@ const express         = require("express");
 const app             = express();
 const port            = 3000;
 const reactViews      = require("express-react-views");
+const mongoose        = require("mongoose");
 
 
 
@@ -40,7 +41,6 @@ app.get("/logs/new", (req, res) => {
 app.post("/logs", (req, res) => {
     req.body.shipIsBroken = req.body.shipIsBroken === "on" ? true : false;
     res.send(req.body);
-
 });
 
 
