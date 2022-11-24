44fc3d3072125cc68bfa91d13ab5315cdebcb351
banksjt21 <113642612+banksjt21@users.noreply.github.com>
2022-11-23 20:21:28 -0500

    Added MongoDB connection

--- Homework/W9D2/captains-log/server.js
+++ Homework/W9D2/captains-log/server.js
@@ -12,6 +12,21 @@ const mongoose        = require("mongoose");
 
 
 /*  ===========================================================================
+//  MONGOOSE
+//  =======================================================================  */
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+mongoose.connection.once("open", () => {
+    console.log("Connected to MongoDB");
+});
+
+
+
+
+/*  ===========================================================================
 //  VIEW ENGINE
 //  =======================================================================  */
 app.set("view engine", "jsx");
