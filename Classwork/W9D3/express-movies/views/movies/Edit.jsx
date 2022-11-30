37ee8290cb08fa6fe1e8380eac77db4a730aa1bf
banksjt21 <113642612+banksjt21@users.noreply.github.com>
2022-11-30 16:02:50 -0500

    Added today's classwork

---
+++ Classwork/W9D3/express-movies/views/movies/Edit.jsx
@@ -0,0 +1,46 @@
+const React = require("react");
+const DefaultLayout = require("../Default");
+
+class Edit extends React.Component {
+    render() {
+        const { movie } = this.props;
+        return (
+            <DefaultLayout>
+                <a href="/movies/">See All Movies</a>
+                <form action={`/movies/${movie._id}?_method=PUT`} method="POST">
+                    <fieldset>
+                        <legend>Edit Movie</legend>
+                        <label>
+                            TITLE:<input type="text" name="title" placeholder="Enter movie title" defaultValue={movie.title} />
+                        </label>
+
+                        <label>
+                            GENRE:<input type="text" name="genre" placeholder="enter genre"  defaultValue={movie.genre} />
+                        </label>
+
+                        <label>RELEASE DATE:<input type="text" name="releaseDate"  defaultValue={movie.releaseDate}/> </label>
+
+                        <label>LENGTH:<input type="number" name="length"  defaultValue={movie.length} /></label>
+
+                        <label>POSTER:<input type="text" name="poster"  defaultValue={movie.poster}/></label>
+
+                        <label>DIRECTOR:<input type="text" name="director"  defaultValue={movie.director}/></label>
+
+                        <label>RATING:<input type="text" name="rating"  defaultValue={movie.rating}/></label>
+
+                        <label>CAST: (Please separate case members with "|"<input type="text" name="cast"  defaultValue={movie.cast.join("|")}/></label>
+                       
+                        {
+                            movie.watchAgain ?
+                            <label>WATCH AGAIN :<input type="checkbox" name="watchAgain" checked /></label> :
+                            <label>WATCH AGAIN :<input type="checkbox" name="watchAgain" /> </label>
+                        }
+                    </fieldset>
+                    <input type="submit" value="Edit Movie" />
+                </form>
+            </DefaultLayout>
+        )
+    }
+}
+
+module.exports = Edit;
---
+++ Classwork/W9D3/express-movies/views/movies/Show.jsx
@@ -0,0 +1,40 @@
+const React = require("react");
+const DefaultLayout = require("../Default");
+
+class Show extends React.Component {
+    render() {
+        const { movie } = this.props;
+        return (
+            // Inside the render of the class
+            <DefaultLayout>
+                <div>
+                    <article>
+                        <img src={movie.poster} alt="" width="200" height="300" />
+                        <h2>{movie.title}</h2>
+                        <p>{movie.genre} | {movie.releaseDate ? movie.releaseDate : "N/A"}</p>
+                        <p>{movie.rating}</p>
+                        <p>{movie.watchAgain ? "I would watch again." : "You gotta pay me to watch it."}</p>
+                        <h4>{movie.director}</h4>
+
+                        Cast:
+                        <ul>
+                            {movie.cast.map((star, i) => {
+                                return (
+                                    <li key={i}>{star}</li>
+                                )
+                            })}
+                        </ul>
+
+                        <a href={`/movies/${movie._id}/Edit`}><button>Edit</button></a>
+                        <form action={`/movies/${movie._id}?_method=DELETE`} method="POST">
+                            <input type="submit" value="Delete" />
+                        </form>
+                        <a href="/movies/"><button>Back to Main</button></a>
+                    </article>
+                </div>
+            </DefaultLayout>
+        )
+    }
+}
+
+module.exports = Show;
