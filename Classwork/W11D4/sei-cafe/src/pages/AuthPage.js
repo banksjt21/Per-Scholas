9f60b58f3850fc893f66d1664926697dde32a4e0
banksjt21 <113642612+banksjt21@users.noreply.github.com>
2022-12-14 15:11:07 -0500

    started W11D4 lesson

---
+++ Classwork/W11D4/sei-cafe/src/pages/AuthPage.js
@@ -0,0 +1,10 @@
+import SignUpFrom from "../components/SignUpForm";
+
+export default function AuthPage() {
+    return(
+        <main>
+            <h1>AuthPage</h1>
+            <SignUpFrom />
+        </main>
+    )
+}
