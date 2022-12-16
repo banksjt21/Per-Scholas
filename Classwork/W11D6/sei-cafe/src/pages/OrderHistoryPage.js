45ac686c6152fb309b19789763edae485bda99ce
banksjt21 <113642612+banksjt21@users.noreply.github.com>
2022-12-16 17:27:49 -0500

    display login expiration date on OrderHistoryPage

--- Classwork/W11D6/sei-cafe/src/pages/OrderHistoryPage.js
+++ Classwork/W11D6/sei-cafe/src/pages/OrderHistoryPage.js
@@ -1,16 +1,24 @@
-import { checkToken } from "../utilities/users-service";
+import { checkToken } from "../utilities/users-service"
+import { useState } from "react"
 
-export default function OrderHistoryPage(props) {
-
-    const handleCheckToken = async () => {
-        const expDate = await checkToken();
+export default function OrderHistoryPage (props) {
+    
+    const [expDate,setExpDate] = useState('')
+    const handleCheckToken  = async () => {
+       const expDate = await checkToken()
+       setExpDate(expDate)
     }
-
 
     return (
         <>
-            <h1>OrderHistoryPage</h1>
-            <button onClick={handleCheckToken}>Check When My Login Expires</button>
+        <h1>OrderHistoryPage</h1>
+        <button onClick={handleCheckToken}>Check When My Login Expires</button>
+        {
+            expDate?
+            <p>{expDate.toLocaleString()}</p>
+            :
+            ""
+        }
         </>
     )
 }
